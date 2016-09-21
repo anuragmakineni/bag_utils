720154d808e0a6a6ded224904f8075277aeb5687
Anurag Makineni <anurag.makineni@gmail.com>
2016-09-21 01:35:41 -0400

    added position command script

--- scripts/position_cmd.py
+++ scripts/position_cmd.py
@@ -16,13 +16,13 @@ odom_y = np.empty((0))
 odom_z = np.empty((0))
 
 bag = rosbag.Bag('/home/anuragmakineni/Desktop/cc_1.bag')
-topic = '/pico03/odom'
+topic = '/pico03/position_cmd'
 
 for topic, msg, t in bag.read_messages(topics=[topic]):
 
-    odom_x = np.append(odom_x, msg.pose.pose.position.x)
-    odom_y = np.append(odom_y, msg.pose.pose.position.y)
-    odom_z = np.append(odom_z, msg.pose.pose.position.z)
+    odom_x = np.append(odom_x, msg.position.x)
+    odom_y = np.append(odom_y, msg.position.y)
+    odom_z = np.append(odom_z, msg.position.z)
 
 
 #odom
@@ -30,7 +30,7 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.plot(odom_x, odom_y, odom_z)
 
-topic = '/pico04/odom'
+topic = '/pico04/position_cmd'
 
 odom_x = np.empty((0))
 odom_y = np.empty((0))
@@ -38,13 +38,13 @@ odom_z = np.empty((0))
 
 for topic, msg, t in bag.read_messages(topics=[topic]):
 
-    odom_x = np.append(odom_x, msg.pose.pose.position.x)
-    odom_y = np.append(odom_y, msg.pose.pose.position.y)
-    odom_z = np.append(odom_z, msg.pose.pose.position.z)
+    odom_x = np.append(odom_x, msg.position.x)
+    odom_y = np.append(odom_y, msg.position.y)
+    odom_z = np.append(odom_z, msg.position.z)
 
 ax.plot(odom_x, odom_y, odom_z)
 
-topic = '/pico05/odom'
+topic = '/pico05/position_cmd'
 
 odom_x = np.empty((0))
 odom_y = np.empty((0))
@@ -52,9 +52,9 @@ odom_z = np.empty((0))
 
 for topic, msg, t in bag.read_messages(topics=[topic]):
 
-    odom_x = np.append(odom_x, msg.pose.pose.position.x)
-    odom_y = np.append(odom_y, msg.pose.pose.position.y)
-    odom_z = np.append(odom_z, msg.pose.pose.position.z)
+    odom_x = np.append(odom_x, msg.position.x)
+    odom_y = np.append(odom_y, msg.position.y)
+    odom_z = np.append(odom_z, msg.position.z)
 
 ax.plot(odom_x, odom_y, odom_z)
 
